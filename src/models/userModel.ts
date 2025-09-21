26529d633d18c4d47895ea1c4c91b11cad9b8d5e
PardiniO <pardinioriana@gmail.com>
2025-09-21 01:03:26 -0300

    Added model del usuario

---
+++ src/models/userModel.ts
@@ -0,0 +1,72 @@
+import { ResultSetHeader } from "mysql2";
+import bcrypt from "bcrypt";
+import { pool } from "../config/db";
+import { IUsuario } from "../interfaces/userIterface";
+import * as userQuery from "../queries/userQueries";
+
+export class UsuarioModel {
+    static async create(usuario: IUsuario): Promise<number> {
+        const { nombre, email, contraseña, rol } = usuario;
+        const [result] = await pool.query<ResultSetHeader>(
+            userQuery.INSERT_USER, 
+            [nombre, email, contraseña, rol]
+        );
+        return result.insertId;
+    }
+
+    static async findAll(): Promise<IUsuario[]>{
+        const [rows] = await pool.query<IUsuario[]>(userQuery.SELECT_ALL_USERS);
+        return rows as IUsuario[];
+    };
+
+    static async findById(id: number): Promise<IUsuario | null>{
+        const [rows] = await pool.query<IUsuario[]>(
+            userQuery.SELECT_USER_BY_ID, [id]
+        );
+        return rows.length > 0 ? (rows[0] as IUsuario): null;
+    }
+
+    static async findByEmail(email: string): Promise<IUsuario | null> {
+        const [rows] = await pool.query<IUsuario[]>(
+            userQuery.SELECT_USER_BY_EMAIL, [email]
+        );
+        return rows.length > 0 ? (rows[0] as IUsuario): null;
+    }
+
+    static async findByRol(rol: string): Promise<IUsuario[]>{
+        const [rows] = await pool.query<IUsuario[]>(
+            userQuery.SELECT_USER_BY_ROLE, [rol]
+        );
+        return rows as IUsuario[];
+    }
+
+    static async update(id: number, usuario: IUsuario): Promise<boolean> {
+        const { nombre, email, contraseña, rol } = usuario;
+        const [result] = await pool.query<ResultSetHeader>(
+            userQuery.UPDATE_USER, [nombre, email, contraseña, rol, id]
+        );
+        return result.affectedRows > 0;
+    }
+
+    static async updatePass(id: number, newPass: string): Promise<boolean> {
+        const hash = await bcrypt.hash(newPass, 10);
+        const [result] = await pool.query<ResultSetHeader>(
+            userQuery.UPDATE_PASSWORD, [hash, id]
+        );
+        return result.affectedRows > 0;
+    }
+
+    static async count(): Promise<IUsuario[]> {
+        const [rows] = await pool.query(
+            userQuery.COUNT_USER
+        );
+        return (rows as IUsuario[])[0].total;
+    }
+
+    static async exists(email: string): Promise<boolean> {
+        const [rows] = await pool.query(
+            userQuery.EXISTS_USER, [email]
+        );
+        return (rows as IUsuario[]).length > 0;
+    }
+}
--- src/queries/userQueries.ts
+++ src/queries/userQueries.ts
@@ -9,15 +9,26 @@ export const SELECT_USER_BY_ID = BASE_SELECT_USER + `WHERE id = ?`;
 
 export const SELECT_USER_BY_EMAIL = BASE_SELECT_USER + `WHERE email = ?`;
 
+export const SELECT_USER_BY_ROLE = BASE_SELECT_USER + `WHERE rol = ?`;
+
 export const INSERT_USER = `
     INSER INTO usuario (nombre, email, contraseña, rol)
     VALUES (?, ?, ?, ?)
+`;
+
+export const COUNT_USER = `
+    SELECT COUNT(*) as total
+    FROM usuario
 `;
 
 export const UPDATE_USER = `
     UPDATE usuario SET nombre = ?, rol = ?, WHERE id = ?
 `;
 
-export const DELETE_USER = `
-    DELETE FROM usuario WHERE id = ?
+export const UPDATE_PASSWORD = `
+    UPDATE usuario SET contraseña = ? WHERE id = ?
+`;
+
+export const EXISTS_USER = `
+    SELECT 1 FROM usuario WHERE email = ? LIMIT 1
 `;
