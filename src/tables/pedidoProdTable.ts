e377616cd3b5359ebdfa748decbab469b4a6a315
PardiniO <pardinioriana@gmail.com>
2025-09-22 20:39:38 -0300

    Fix sintaxis de queries

--- src/tables/pedidoProdTable.ts
+++ src/tables/pedidoProdTable.ts
@@ -1,9 +1,9 @@
 export const PEDIDO_PROD_TABLE = `
-    CREATE TABLE IF EXISTS pedido_p (
+    CREATE TABLE IF NOT EXISTS pedido_p (
         id INT AUTO_INCREMENT PRIMARY KEY,
         id_pedido INT NOT NULL,
         id_producto INT NOT NULL,
         cantidad INT NOT NULL,
-        precio_unitario DECIMAL(10,2) NOT NUL,
+        precio_unitario DECIMAL(10,2) NOT NULL
     );
 `;
--- src/tables/productoTable.ts
+++ src/tables/productoTable.ts
@@ -1,7 +1,7 @@
 export const PROD_TABLE = `
-    CREATE TABLE IF NOT EXIST producto (
+    CREATE TABLE IF NOT EXISTS producto (
         id INT AUTO_INCREMENT PRIMARY KEY,
-        nombre VARCHAR(100) NOT NUL,
+        nombre VARCHAR(100) NOT NULL,
         precio_unitario DECIMAL(10,2) NOT NULL,
         stock INT NOT NULL
     );
