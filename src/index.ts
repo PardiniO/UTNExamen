a01353f1e1cfcc8aa54d7e4b34a0892ad7f5e70d
PardiniO <pardinioriana@gmail.com>
2025-09-22 00:51:16 -0300

    Integración de producto al index

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express, { Application } from "express";
 import userRoutes from "./routes/userRoutes";
+import productRoutes from "./routes/productRoutes";
 
 const app: Application = express();
 app.use(express.json());
 
 app.use('/usuarios', userRoutes);
+app.use('/productos', productRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
