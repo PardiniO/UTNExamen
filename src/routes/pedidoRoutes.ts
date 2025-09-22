ccb87f8a2041fc2b7c0b75d868b1b9d055c80a44
PardiniO <pardinioriana@gmail.com>
2025-09-22 01:43:28 -0300

    Added rutas de pedido

---
+++ src/routes/pedidoRoutes.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import * as pedidoController from "../controllers/pedidoController";
+import { authGuard, roleGuard } from "../middlewares/guardMiddleware";
+
+const router = Router();
+
+router.get('/', authGuard, pedidoController.getAllPedidos);
+router.get('/:id', authGuard, pedidoController.getPedidoById);
+
+router.post('/', authGuard, roleGuard('admin', 'superAdmin'), pedidoController.createPedido);
+router.put('/:id', authGuard, roleGuard('admin', 'superAdmin'), pedidoController.updatePedido);
+router.delete('/:id', authGuard, roleGuard('admin', 'superAdmin'), pedidoController.deletePedido);
+
+export default router;
